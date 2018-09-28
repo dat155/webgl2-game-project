199e3265a087719b01093e1b2ffe459c14d12f76
oskarbraten <oskar.braten@outlook.com>
2018-09-28 15:34:34 +0200

    Fix bug in getMinMax.

--- js/engine/primitives/utils.js
+++ js/engine/primitives/utils.js
@@ -2,11 +2,11 @@ import { vec3 } from '../lib/gl-matrix.js';
 
 export const getMinMax = (vertices) => {
 
-    let min = vec3.fromValues(0, 0, 0);
-    let max = vec3.fromValues(0, 0, 0);
+    let min = vec3.fromValues(+Infinity, +Infinity, +Infinity);
+    let max = vec3.fromValues(-Infinity, -Infinity, -Infinity);
 
-    for (let i = 0; i < (vertices.length / 3); i += 3) {
-
+    for (let i = 0; i < vertices.length; i += 3) {
+        
         min[0] = Math.min(min[0], vertices[i + 0]);
         min[1] = Math.min(min[1], vertices[i + 1]);
         min[2] = Math.min(min[2], vertices[i + 2]);
