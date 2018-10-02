e63e531ba64a8c5532cdffdb54587e097d20e03f
oskarbraten <oskar.braten@outlook.com>
2018-10-02 20:40:23 +0200

    Change comment

--- js/engine/material/BasicShader.js
+++ js/engine/material/BasicShader.js
@@ -9,7 +9,7 @@ export default class BasicShader extends Shader {
     constructor(gl, material) {
         super(gl, material, vertexShaderSource, fragmentShaderSource);
 
-        // get specific uniform locations.
+        // get uniform locations specific to this shader.
         this.uniformLocations.color = gl.getUniformLocation(this.program, 'color');
         this.uniformLocations.map = gl.getUniformLocation(this.program, 'map');
 
