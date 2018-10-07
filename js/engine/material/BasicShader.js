79dc191bedc966c07c4776a34e8650e55a5e41e6
oskarbraten <oskar.braten@outlook.com>
2018-10-08 00:16:45 +0200

    Modify let to const

--- js/engine/material/BasicShader.js
+++ js/engine/material/BasicShader.js
@@ -2,8 +2,8 @@
 import Shader from './Shader.js';
 import fetchShaderSource from '../lib/fetchShaderSource.js';
 
-let vertexShaderSource = fetchShaderSource('shaders/basic-vertex-shader.glsl');
-let fragmentShaderSource = fetchShaderSource('shaders/basic-fragment-shader.glsl');
+const vertexShaderSource = fetchShaderSource('shaders/basic-vertex-shader.glsl');
+const fragmentShaderSource = fetchShaderSource('shaders/basic-fragment-shader.glsl');
 
 export default class BasicShader extends Shader {
     constructor(gl, material) {
