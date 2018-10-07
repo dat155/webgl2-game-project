221f3694b01e563c9a26476e586041801563d83a
oskarbraten <oskar.braten@outlook.com>
2018-10-08 00:15:51 +0200

    Add getter methods for lights and meshes

--- js/engine/Scene.js
+++ js/engine/Scene.js
@@ -1,8 +1,14 @@
 import Node from './Node.js';
 
+import Mesh from './mesh/Mesh.js';
+import Light from './light/Light.js';
+import { vec4 } from './lib/gl-matrix.js';
+
 export default class Scene extends Node {
     constructor() {
         super();
+
+        this.ambient = vec4.fromValues(0.2, 0.2, 0.2, 1.0);
     }
 
     /**
@@ -10,5 +16,33 @@ export default class Scene extends Node {
     */
     update() {
         this.tick();
+    }
+
+    static getMeshNodes(node) {
+
+        if (node instanceof Mesh) {
+            return [node].concat(...node.children.map(this.getMeshNodes.bind(this)));
+        } else {
+            return [].concat(...node.children.map(this.getMeshNodes.bind(this)));
+        }
+
+    }
+
+    static getLightNodes(node) {
+
+        if (node instanceof Light) {
+            return [node].concat(...node.children.map(this.getLightNodes.bind(this)));
+        } else {
+            return [].concat(...node.children.map(this.getLightNodes.bind(this)));
+        }
+
+    }
+
+    getMeshNodes() {
+        return this.constructor.getMeshNodes(this);
+    }
+
+    getLightNodes() {
+        return this.constructor.getLightNodes(this);
     }
 }
